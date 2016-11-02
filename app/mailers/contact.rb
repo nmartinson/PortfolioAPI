0f6d00a4032ea7e98a1ccb256687b05eb06db614
Nick Martinson <nickmartinson986@gmail.com>
2016-11-02 14:58:34 -0700

    updated email settings

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActionMailer::Base
-  default :from => '10khourbot@gmail.com'
+  default :from => 'photoinquiries@boundless-journey.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_contact_email(params)
